4c50df6eba23982b995f278eb91ce5f862310b90
Rafael Rodrigues <rafaelrodripaz@gmail.com>
2019-01-05 14:49:06 -0200

    Updates in parser.py

--- db/parser.py
+++ db/parser.py
@@ -3,9 +3,17 @@ import sys
 import os
 import random
 
-filename = sys.argv[2]+"-"+str(os.times()[4])+"-"+str(random.randint(1,99999))
+filename = sys.argv[2]+"-"+str(os.times()[4])+"-"+str(random.randint(1,99999))+".txt"
 os.system("copy NUL "+filename)
-res = jsbeautifier.beautify_file(sys.argv[1])
+
+parser = open(sys.argv[1],"r")
+
+body = parser.read().split('\r\n\r\n')
+with open(filename,'w') as f:
+    f.write(body[1])
+parser.close()
+
+res = jsbeautifier.beautify_file(filename)
 with open(filename,'w') as f:
     f.write(res)
 
