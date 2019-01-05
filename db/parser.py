050db84d6126f34b250d7309533538d3fd067912
Rafael Rodrigues <rafaelrodripaz@gmail.com>
2019-01-05 14:54:47 -0200

    Windows support

--- db/parser.py
+++ db/parser.py
@@ -4,17 +4,17 @@ import os
 import random
 
 filename = sys.argv[2]+"-"+str(os.times()[4])+"-"+str(random.randint(1,99999))+".txt"
-os.system("copy NUL "+filename)
+os.system("copy NUL db/"+filename)
 
 parser = open(sys.argv[1],"r")
 
 body = parser.read().split('\r\n\r\n')
-with open(filename,'w') as f:
+with open("db/"+filename,'w') as f:
     f.write(body[1])
 parser.close()
 
-res = jsbeautifier.beautify_file(filename)
-with open(filename,'w') as f:
+res = jsbeautifier.beautify_file("db/"+filename)
+with open("db/"+filename,'w') as f:
     f.write(res)
 
 print filename
